422b55d819d6064e7887a5b0556baca329f79fde
ScriptItWithCode <62032154+ScriptItWithCode@users.noreply.github.com>
2020-09-04 15:46:25 +0200

    .

--- package.lua
+++ package.lua
@@ -3,8 +3,20 @@ return {
   version = "1.0.0",
   description = "For the discord",
   dependencies = {
-    "luvit/luvit",
-	"creationix/coro-http",
-	"SinisterRectus/discordia"
+  "luvit/luvit",
+  "SinisterRectus/discordia",
+  "creationix/base64",
+  "creationix/coro-channel",
+  "creationix/coro-http",
+  "creationix/coro-net",
+  "creationix/coro-websocket",
+  "creationix/coro-wrapper",
+  "creationix/pathjoin",
+  "creationix/sha1",
+  "creationix/websocket-codec",
+  "luvit/http-codec",
+  "luvit/resource",
+  "luvit/secure-socket",
+  
   },
 }
