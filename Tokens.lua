c213684978ad28d8f4e10d21a8173944baf63030
ScriptItWithCode <62032154+ScriptItWithCode@users.noreply.github.com>
2020-10-12 16:39:52 +0200

    tokens

---
+++ Tokens.lua
@@ -0,0 +1,5 @@
+return {
+    Client = "NjY1ODg2ODkyODAxMTMwNTE2.XhsJfg.qnPeLDzrCLYgOzIOsz959ef24Oo",
+    MusicClient = "NzUwMjQzMTgwNTE5MjI3NDQy.X03saQ.xn6fidNQ4vTIRLvVrzKvw7db2uc",
+    MilkClient = "NzY1MTAyNjE4NDM1MTI1MjU5.X4P7Vw.pYvV6S_NjFDZrHxDizwQP6a-Ehw"
+}
