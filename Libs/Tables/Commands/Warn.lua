97d2ed7bf94a393c467e9d906bf6392c1e86c7da
ScriptItWithCode <62032154+ScriptItWithCode@users.noreply.github.com>
2020-10-13 13:26:11 +0200

    Update Warn.lua

--- Libs/Tables/Commands/Warn.lua
+++ Libs/Tables/Commands/Warn.lua
@@ -3,12 +3,12 @@ return function(Data)
 
     if MentionedArray[1] then
         if Data.Args[1] then
-            Data.PreMSG:setContent("**" .. MentionedArray[1].tag .. "** is gewarned: " .. Data.Args[1])
+            Data.PreMSG:setContent("**" .. MentionedArray[1].tag .. "** is gewarned: " .. table.concat(Data.Args, " "))
         else
             Data.PreMSG:setContent("**" .. MentionedArray[1].tag .. "** is gewarned: Geen Rede Gegeven")
         end
 
-        local Reason = Data.Args[1] or "Geen Rede"
+        local Reason = table.concat(Data.Args, " ") or "Geen Rede"
 
         local ToSend = {
             content = "",
@@ -16,7 +16,7 @@ return function(Data)
             {
                 title = "User Warn",
                 
-                description = "**Moderator** | `" .. Data.Author.tag .. "`\n**Overtreder** | `" .. MentionedArray[1].tag .. "`\n**Rede** | " .. Reason .. "\n**Time** | `" .. os.date("%c") .. "`",
+                description = "**Moderator** | `" .. Data.Author.tag .. "`\n**Overtreder** | `" .. MentionedArray[1].tag .. "`\n**Rede** | `" .. Reason .. "`\n**Time** | `" .. os.date("%c") .. "`",
 
 
                 color = 0xff7e00
