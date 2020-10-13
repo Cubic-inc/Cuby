32afaa5dc150ee0d5fdab73a718d5729f70c218d
ScriptItWithCode <62032154+ScriptItWithCode@users.noreply.github.com>
2020-10-13 11:22:54 +0200

    milk update

--- Libs/Tables/Commands.lua
+++ Libs/Tables/Commands.lua
@@ -79,15 +79,15 @@ return {
             if not Data.Args[1] then
                 Data.PreMSG:setContent("Geen sub commando gegeven")
             elseif string.lower(Data.Args[1]) == "drink" and GlobalData.Milk == true then
-                Data.PreMSG:setContent("Slurp Slurp")
+                Data.PreMSG:setContent(":milk: Slurp Slurp ")
                 GlobalData.Milk = false
             elseif string.lower(Data.Args[1]) == "drink" and GlobalData.Milk == false then
-                Data.PreMSG:setContent("Het Glas is leeg")
+                Data.PreMSG:setContent(":milk: Het Glas is leeg :frowning2: ")
             elseif string.lower(Data.Args[1]) == "fill" and GlobalData.Milk == false then
-                Data.PreMSG:setContent("Klok Klok")
+                Data.PreMSG:setContent(":milk: Klok Klok")
                 GlobalData.Milk = true
             elseif string.lower(Data.Args[1]) == "fill" and GlobalData.Milk == true then
-                Data.PreMSG:setContent("Het glas is al vol")
+                Data.PreMSG:setContent(":milk: Het glas is al vol :frowning2: ")
             end
 
         end
