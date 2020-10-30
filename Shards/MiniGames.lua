d836e1103302c95fa5495f550411cbab4e39922e
ScriptItWithCode <62032154+ScriptItWithCode@users.noreply.github.com>
2020-10-30 16:29:38 +0100

    Update MiniGames.lua

--- Shards/MiniGames.lua
+++ Shards/MiniGames.lua
@@ -1,7 +1,7 @@
 return function(Data)
 
     local Json = require("json")
-    local PP = require('pretty-print')
+    --local PP = require('pretty-print')
 
     Client = Data.Client
 
@@ -16,19 +16,41 @@ return function(Data)
         Amongo = Guild:getRole("769574625553678366")
     }
 
-    local BeginAmounts = {
-        Amongo = #Messages.Amongo.reactions:toArray()
-
+    local Reactions = {
+        Amongo = Messages.Amongo.reactions:toArray()[1]
     }
 
-    --print(Data.Libs.Code.TableToString(Messages.Amongo.reactions:toArray()))
+    local BeginAmounts = {
+        Amongo = Reactions.Amongo.count
+    }
+
+    print(Messages.Amongo.reactions:toArray()[1].count)
+    for i, v in pairs(Messages.Amongo.reactions:toArray()) do
+        print(v)
+    end
 
     Client:on("reactionAdd", function(Reaction, UserId)
     
         local User = Client:getUser(UserId)
         local Member = Guild:getMember(UserId)
 
+        if BeginAmounts.Amongo ~= Reactions.Amongo.count then
+            Member:addRole("769574625553678366")
+            BeginAmounts.Amongo = Reactions.Amongo.count 
+        end
         
+        
+    end)
+
+    Client:on("reactionRemove", function(Reaction, UserId)
+    
+        local User = Client:getUser(UserId)
+        local Member = Guild:getMember(UserId)
+
+        if BeginAmounts.Amongo ~= Reactions.Amongo.count then
+            Member:removeRole("769574625553678366")
+            BeginAmounts.Amongo = Reactions.Amongo.count 
+        end
         
         
     end)
