5c41eabadc14e87bdeb8991f98822435dec9d6af
ScriptItWithCode <62032154+ScriptItWithCode@users.noreply.github.com>
2020-11-19 15:18:47 +0100

    still working

--- libs/Code/StickyCommands.lua
+++ libs/Code/StickyCommands.lua
@@ -2,6 +2,53 @@ return function(Data)
 
     local Handler = Data.CommandHandler
     local Base = require("Code/Save"):GetDatabase("botinfo")
+    local Client = Data.Client
+
+    local Stickies = Base:GetAsync("Stickies")
+
+    function Write()
+        Base:PostAsync("Stickies", Stickies)
+    end
+
+    Client:on("messageCreate", function(MSG)
+        
+
+        if MSG.author.id == Client.user.id then return end
+
+        if Stickies[MSG.channel.id] then
+
+            local Channel = Client:getChannel(MSG.channel.id)
+            local Last = Channel:getMessage(Stickies[MSG.channel.id].LastMessage)
+            if Last then
+                Last:delete()
+            else
+                print("no last")
+            end
+
+            if Stickies[MSG.channel.id].Enabled == true then
+
+
+
+                local Sticky = Stickies[MSG.channel.id]
+
+                local Send = MSG:reply({embed = {
+
+                    title = Sticky.Title,
+                    description = Sticky.Desc,
+                    fields = Sticky.Fields
+
+                }})
+
+                if Send then
+                    Stickies[MSG.channel.id].LastMessage = Send.id
+                end
+
+            end
+        end
+
+        Base:PostAsync("Stickies", Stickies)
+    
+    end)
 
     --Base:PostAsync("Stickies", {})
 
@@ -33,25 +80,108 @@ return function(Data)
     BoolEnable:SetReq(true)
 
     EnableCommand:SetFunction(function(MSG, Args, Raw)
+        local ToEnable
 
-        
-
-        if Args[i] == "on" then
-            local BoolEnable = true
-        elseif Args[i] == "off" then
-            local BoolEnable = false
+        if Args[1] == "on" then
+            ToEnable = true
+            MSG:reply("Sticky is enabled!")
+        elseif Args[1] == "off" then
+            ToEnable = false
+            MSG:reply("Sticky is enabled!")
         else
-            MSG:reply("aan of uit?")
+            MSG:reply("on or off")
             return
-        end
+        end        
     
-        local Stickies = Base:GetAsync("Stickies")
         if Stickies[MSG.channel.id] then
-            local Sticky = Stickies[MSG.channel.id]
+            Stickies[MSG.channel.id].Enabled = ToEnable
         else
             Stickies[MSG.channel.id] = {}
-            local Sticky = Stickies[MSG.channel.id]
+            Stickies[MSG.channel.id].Enabled = ToEnable
         end
+
+        Write()
+
+    end)
+
+    local TitleCommand = Handler.New()
+    TitleCommand:SetName("Stickytitle")
+    TitleCommand:SetMinPerm("Owner")
+
+    local NewTitle = TitleCommand:NewArg()
+    NewTitle:SetName("New Title")
+    NewTitle:SetType("String")
+    NewTitle:SetReq(true)
+
+    TitleCommand:SetFunction(function(MSG, Args, Raw)
+        
+        if Stickies[MSG.channel.id] then
+            Stickies[MSG.channel.id].Title = table.concat(Raw, " ")
+        else
+            Stickies[MSG.channel.id] = {Fields = {}}
+            Stickies[MSG.channel.id].Title = table.concat(Raw, " ")
+        end
+
+        MSG:reply("Title set!")
+
+        Write()
+
+    end)
+
+    local DescCommand = Handler.New()
+    DescCommand:SetName("Stickydesc")
+    DescCommand:SetMinPerm("Owner")
+
+    local NewDesc = DescCommand:NewArg()
+    NewDesc:SetName("New Desc")
+    NewDesc:SetType("String")
+    NewDesc:SetReq(true)
+
+    DescCommand:SetFunction(function(MSG, Args, Raw)
+        
+        if Stickies[MSG.channel.id] then
+            Stickies[MSG.channel.id].Desc = table.concat(Raw, " ")
+        else
+            Stickies[MSG.channel.id] = {Fields = {}}
+            Stickies[MSG.channel.id].Desc = table.concat(Raw, " ")
+        end
+
+        MSG:reply("Desc set to: " .. table.concat(Raw, " "))
+
+        Write()
+
+    end)
+
+    local FieldTitleCommand = Handler.New()
+    FieldTitleCommand:SetName("FieldTitle")
+    FieldTitleCommand:SetMinPerm("Owner")
+
+    local FieldNumber = FieldTitleCommand:NewArg()
+    FieldNumber:SetName("NumberID")
+    FieldNumber:SetType("Number")
+    FieldNumber:SetReq(true)
+
+    local FieldTitle = FieldTitleCommand:NewArg()
+    FieldTitle:SetName("New-title")
+    FieldTitle:SetType("String")
+    FieldTitle:SetReq(false)
+
+    FieldTitleCommand:SetFunction(function(MSG, Args, Raw)
+
+        print(type(Args[1]))
+        
+        if Stickies[MSG.channel.id] then
+            Stickies[MSG.channel.id].Fields = {}
+            Stickies[MSG.channel.id].Fields[Args[1]] = {}
+            Stickies[MSG.channel.id].Fields[Args[1]].name = table.concat(Raw, " ")
+        else
+            Stickies[MSG.channel.id] = {}
+            Stickies[MSG.channel.id].Fields[Args[1]].name = table.concat(Raw, " ")
+        end
+
+        MSG:reply("Desc set to: " .. table.concat(Raw, " "))
+
+        Write()
 
     end)
 
