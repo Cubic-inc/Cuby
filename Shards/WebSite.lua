5e319e63ace1c6b1f24db7a626ac8f5aa130872d
ScriptItWithCode <62032154+ScriptItWithCode@users.noreply.github.com>
2020-11-19 15:18:26 +0100

    ok

--- Shards/WebSite.lua
+++ Shards/WebSite.lua
@@ -2,7 +2,7 @@ return function(Data)
 
   local Tokens = require("././Tokens")
   local LoadPage = require("Code/Website/RenderPage")
-
+  local Http = require("coro-http")
   local App = require('weblit-app')
 
 
@@ -50,15 +50,9 @@ return function(Data)
   }, function (req, res)
     
     local Parms = require("Code/Website/GetParams")(req.path)
-
-    print(Parms)
-
-    for i, v in pairs(Parms) do print(i, v) end
-
     local Replace = {message = Parms.message, reffer = Parms.reffer, reffertext = Parms.text}
 
     res.body = LoadPage("././WebsitePages/Succes.html", Replace)
-
     res.headers["Content-Type"] = "text/html"
     res.code = 200
   end)
@@ -68,37 +62,45 @@ return function(Data)
     method = "GET", -- Filter on HTTP verb
     path = "/partner", -- Filter on url patterns and capture some parameters.
   }, function (req, res)
-    
     res.body = LoadPage("././WebsitePages/Partner.html")
-
-    res.headers["Content-Type"] = "text/html"
-    res.code = 200
-  end)
-
-  App.route({method = "GET", path = "/login/done",}, function (req, res)
-
-    local Code = require("Code/Website/GetParams")(req.path)
-    local Http = require("coro-http")
-
-    if Code then
-
-      
-
-    end
-
-    res.body = Code.code
-
-    
-
     res.headers["Content-Type"] = "text/html"
     res.code = 200
   end)
 
   App.route({method = "GET", path = "/login",}, function (req, res)
 
-    local Url = "https://discord.com/api/oauth2/authorize?client_id=665886892801130516&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin%2Fdone&response_type=code&scope=identify"
+    local Query = require("Code/Website/GetParams")(req.path)
+    local Http = require("coro-http")
 
-    res.body = "<!DOCTYPE html>\n<html>\n<head>\n<meta http-equiv=\"refresh\" content=\"0; url=\'" .. Url .. "\'\" />\n</head>\n</html>"
+    res.body = "err"
+    print(req.path)
+    print(Query.code)
+
+    if Query.code then
+
+      res.body = Query.code
+
+      Params = {
+        grant_type = 'authorization_code',
+        client_id = Tokens.MODAPPKEY or os.getenv("MODAPPKEY"),
+        client_secret = Tokens.MODAPPSECRET or os.getenv("MODAPPSECRET"),
+        redirect_uri = "http://localhost:3000/users/" .. UserID .. "/" .. ActionType,
+        code = Code,
+        scope = "identify"
+      }
+
+      local Http = require("coro-http")
+      
+      
+      local Res, Body = Http.request("POST", "https://discord.com/api/oauth2/token", {{"Content-Type", "application/x-www-form-urlencoded"}}, Query.stringify(Params))
+
+      
+    else
+      local Url = "https://discord.com/api/oauth2/authorize?client_id=665886892801130516&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin&response_type=code&scope=identify"
+      res.body = "<!DOCTYPE html>\n<html>\n<head>\n<meta http-equiv=\"refresh\" content=\"0; url=\'" .. Url .. "\'\" />\n</head>\n</html>"
+    end
+
+    
     res.headers["Content-Type"] = "text/html"
     res.code = 200
   end)
