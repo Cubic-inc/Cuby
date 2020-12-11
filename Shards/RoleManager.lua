1dabd12f45ecca49687d42f3c832fec60fd2c9a0
ScriptItWithCode <62032154+ScriptItWithCode@users.noreply.github.com>
2020-12-11 20:00:32 +0100

    bump money

--- Shards/RoleManager.lua
+++ Shards/RoleManager.lua
@@ -21,14 +21,12 @@ return function(Data)
     Client:on("reactionAdd", function(Reaction, User)
         User = Client:getUser(User)
         if User.bot then return end
-
         if Reaction.message == SOWMessage then
             for i, v in pairs(Emojis) do
                 if v ~= Reaction.emojiName then
                     SOWMessage:removeReaction(v, User.id)
                 end
             end
-            
         end
 
     end)
@@ -36,16 +34,44 @@ return function(Data)
     Client:on("reactionRemove", function(Reaction, User)
         User = Client:getUser(User)
         --if not User.bot then return end
-
         if Reaction.message == SOWMessage and not Reaction.me then
             for i, v in pairs(Emojis) do
                 if v ~= Reaction.emojiName then
                     SOWMessage:addReaction(v)
                 end
             end
-            
         end
 
+    end)
+
+    local Message = Client:getChannel("685066367526895658"):getMessage("786914820003856424")
+
+    Client:on("messageCreate", function(Message)
+        local Channel = Message.channel
+        local User = Message.author
+
+        if User.id == "302050872383242240" then
+
+            if not Message.embed then return end
+            if not Message.embed.image then return end
+
+            if Message.embed.image.url == "https://disboard.org/images/bot-command-image-bump.png" then 
+                --print("yes")
+                --print(Message.embed.description)
+                local UserId = string.sub(Message.embed.description, 3, 20)
+                
+                local Base = require("Code/Save"):GetDatabase("money")
+
+                local CurrentM = Base:GetAsync(UserId) or 0
+                local NewM = CurrentM + 100
+
+                Base:PostAsync(UserId, NewM)
+
+                Message:reply("Thanks for the BUMP <@" .. UserId .. ">! +100$")
+            end
+
+        end
+    
     end)
 
     
